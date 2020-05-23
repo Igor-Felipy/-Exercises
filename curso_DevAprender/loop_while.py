6f3f5deebdb0b8ad9a961bc465f2380c6e7dc5c1
Igor-Felipy <igor.felipy123@gmail.com>
2020-05-23 15:43:40 -0400

    loop while

---
+++ curso_DevAprender/loop_while.py
@@ -0,0 +1,21 @@
+'''
+while alguma_condição:
+    executar_um_codigo_aqui
+'''
+
+preço_celular = 800
+while preço_celular >= 800:
+    print('verificando se o preço do celular mudou')
+print(f'Preço do celular mudou para: {preço_celular}')
+
+percorrer_mais_sites = True
+
+while percorrer_mais_sites == True:
+    print('Percorrendo sites')
+    percorrer_mais_sites = False
+
+
+#laço infinito
+while True:
+    print('Executar uma ação')
+
