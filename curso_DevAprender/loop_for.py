06b594a16eb33b347f28a73f8f1cb03f09a9badb
Igor-Felipy <igor.felipy123@gmail.com>
2020-05-23 15:38:23 -0400

    loop for

---
+++ curso_DevAprender/loop_for.py
@@ -0,0 +1,18 @@
+"""
+for nome_da_variavel_temporaria in quantas_vezes:
+    fazer algo aqui dentro
+"""
+# Quando o programa descobre a quantidade por voce!
+fotos = ['foto1','foto2','foto3','foto4','foto5']
+for  foto in fotos:
+    print(foto)
+
+# Voce ja sabe a quantidade de vezes que deve rodar
+# Função range roda sepre uma vez a menos que o numero especificado
+for volume in range(11):
+    print(f'Aumentando o volume para {volume}')
+
+# aqui voce pode escolher o primeiro e o ultimo digito a ser executado
+for volume in range(1,11):
+    print(f'Aumentando o volume para {volume}')
+
